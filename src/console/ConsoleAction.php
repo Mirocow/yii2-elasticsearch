f795e2338735229e1c375be0250ca94e88a886bf
Mirocow <mr.mirocow@gmail.com>
2018-10-17 01:29:53 +0300

    Added protection against multiple simultaneous runs

--- src/console/ConsoleAction.php
+++ src/console/ConsoleAction.php
@@ -3,11 +3,29 @@ namespace mirocow\elasticsearch\console;
 
 use mirocow\elasticsearch\Module;
 use yii\base\Action;
+use yii\base\Event;
 use yii\console\Controller;
 use yii\helpers\Console;
+use Yii;
 
 class ConsoleAction extends Action
 {
+    public function init()
+    {
+        Event::on(Controller::className(), Controller::EVENT_BEFORE_ACTION, function ($event) {
+            if (!$this->acquireMutex()) {
+                $this->getController()->stderr("This process is already running.\n\n", Console::FG_RED);
+                exit;
+            }
+        });
+
+        Event::on(Controller::className(), Controller::EVENT_AFTER_ACTION, function ($event) {
+            $this->releaseMutex();
+        });
+
+        parent::init();
+    }
+
     /**
      * @return Controller
      */
@@ -38,4 +56,40 @@ class ConsoleAction extends Action
             $this->getController()->stderr(' ' . $message . PHP_EOL);
         }
     }
+
+    /**
+     * @return \yii\mutex\Mutex mutex component
+     */
+    protected function getMutex()
+    {
+        return Yii::$app->get('mutex');
+    }
+
+    /**
+     * Acquires current action lock.
+     * @return bool lock acquiring result.
+     */
+    protected function acquireMutex()
+    {
+        return $this->getMutex()->acquire($this->composeMutexName());
+    }
+
+    /**
+     * Release current action lock.
+     * @return bool lock release result.
+     */
+    protected function releaseMutex()
+    {
+        return $this->getMutex()->release($this->composeMutexName());
+    }
+
+    /**
+     * Composes the mutex name.
+     * @return string mutex name.
+     */
+    protected function composeMutexName()
+    {
+        return get_class($this);
+    }
+
 }
