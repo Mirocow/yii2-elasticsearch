d30cf02d4d463e88151939207ba5e13b9d3db641
mirocow <Mirocow@users.noreply.github.com>
2018-11-02 19:28:23 +0300

    Update Module.php

--- src/Module.php
+++ src/Module.php
@@ -5,7 +5,7 @@ use mirocow\elasticsearch\controllers\index\IndexController;
 
 class Module extends \yii\base\Module
 {
-    const VERSION = '1.0.6';
+    const VERSION = '1.0.8';
 
     const MODULE_NAME = 'elasticsearch';
 
